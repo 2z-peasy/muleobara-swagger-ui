ee30f66051c20d9a0fd48400b212765660ec1d76
shinheekim <dev.shinheekim@gmail.com>
2025-03-24 03:41:13 +0900

    Test Swagger UI

---
+++ swagger-initializer.js
@@ -0,0 +1,21 @@
+window.onload = function() {
+  //<editor-fold desc="Changeable Configuration Block">
+
+  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+  window.ui = SwaggerUIBundle({
+    url: "swagger.json",  // 여기서 swagger.json을 로드하도록 설정
+    dom_id: '#swagger-ui',
+    deepLinking: true,
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    layout: "StandaloneLayout"
+  });
+
+  window.ui = ui;
+  //</editor-fold>
+};
